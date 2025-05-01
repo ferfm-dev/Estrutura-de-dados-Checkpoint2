bdce59e7a537c9eaf404c7dead42134c51a11ace
Fernando <fernandofmelo.dev@gmail.com>
2025-05-01 20:36:23 -0300

    Biblioteca string e função verificar substring adicionadas.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 
 int sequencia_fibonacci() {
     int tamanho = 0;
@@ -32,6 +33,39 @@ int sequencia_fibonacci() {
     return 0;
 }
 
+void verificar_substring() {
+    char palavra1[100], palavra2[100];
+
+    printf("\nDigite a primeira palavra: ");
+    scanf("%s", palavra1);
+
+    printf("\nDigite a segunda palavra: ");
+    scanf("%s", palavra2);
+
+    int len_palavra1 = strlen(palavra1);
+    int len_palavra2 = strlen(palavra2);
+
+    int aux = 0;
+
+    for (int i = 0; i <= len_palavra1 - len_palavra2; i++) {
+        aux = 0;
+        
+        for (int j = 0; j < len_palavra2; j++) {
+            if (palavra1[i + j] == palavra2[j]) {
+                aux++;
+            }
+        }
+        
+        if (aux == len_palavra2) {
+            printf("A segunda string está contida na primeira.");
+            break;
+        }
+    }
+    if (aux != len_palavra2) {
+        printf("A segunda string NÃO está contida na primeira.");
+    }
+}
+
 void menu() {
     int opcao = 0;
     printf("\n==== MENU DE EXERCÍCIOS ====");
@@ -46,9 +80,11 @@ void menu() {
     switch (opcao)
     {
     case 1:
-        sequencia_fibonacci(1);
+        sequencia_fibonacci();
         break;
-    
+    case 4:
+        verificar_substring();
+        break;
     default:
         break;
     }
